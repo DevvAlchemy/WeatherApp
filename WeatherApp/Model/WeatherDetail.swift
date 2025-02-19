d51b8e6f97fdd4cdee8a733a769f1e4d9dc34ba4
Carlito Kabambi <carlito.kabambi@triosstudent.com>
2025-02-19 12:31:45 -0500

    Baisc Weather App w/ Error Handling, Modular design,Concurrent Data loading and Enhanced UI. more animation to come over time

---
+++ WeatherApp/Model/WeatherDetail.swift
@@ -0,0 +1,18 @@
+//
+//  WeatherDetail.swift
+//  WeatherApp
+//
+//  Created by Royal K on 2025-02-19.
+//
+
+import SwiftUI
+
+struct WeatherDetail: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WeatherDetail()
+}
---
+++ WeatherApp/Model/WeatherService.swift
@@ -0,0 +1,18 @@
+//
+//  WeatherService.swift
+//  WeatherApp
+//
+//  Created by Royal K on 2025-02-19.
+//
+
+import SwiftUI
+
+struct WeatherService: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WeatherService()
+}
